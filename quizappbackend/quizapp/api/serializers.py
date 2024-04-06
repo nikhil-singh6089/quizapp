8ef7ace4ccc0f213edf315989e3feb95bb4f9882
nikhil-ub-hard <nikhilsingh6089@gmail.com>
2024-04-06 23:31:47 +0530

    backend added

---
+++ quizappbackend/quizapp/api/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from ..models import Quiz, Question, Answer
+
+class QuizSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quiz
+        fields = '__all__'
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = '__all__'
+
+
---
+++ quizappbackend/quizapp/api/views.py
@@ -0,0 +1,19 @@
+from rest_framework import ModelViewSet
+from ..models import Quiz, Question, Answer
+from .serializers import QuizSerializer, QuestionSerializer, AnswerSerializer
+
+class QuizViewSet(ModelViewSet):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
+
+class QuestionViewSet(ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+class AnswerViewSet(ModelViewSet):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+
+
+
+
